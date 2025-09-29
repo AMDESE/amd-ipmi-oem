356a25a2eadddef40a76d6ba5be028cae5834a00
Raja Sekhar Reddy Gade <RajaSekharReddy.Gade@amd.com>
2025-09-29 06:34:42 -0500

    Implement GetBootStrapAccCredentials ipmi oem cmd
    This commit adds support for the Get Bootstrap Account Credentials
    IPMI command, whenever BIOS/Host demands for the Get BootStrap Account,
    then the BMC will generate unique username and password and send back
    to the BIOS/Host.
    
    Unit Tested the command
    
    Signed-off-by: Raja Sekhar Reddy Gade <RajaSekharReddy.Gade@amd.com>

--- src/amd_oem.hpp
+++ src/amd_oem.hpp
@@ -4,28 +4,23 @@
 #include <cstdint>
 #include <string_view>
 
-namespace amd
-{
+namespace amd {
 
-
-enum ipmi_amd_net_fns
-{
-    NETFN_OEM_AMD = 0x30,
+enum ipmi_amd_net_fns {
+  NETFN_OEM_AMD = 0x30,
 };
 
-enum amd_oem_cmds
-{
-    CMD_OEM_PLATFORM_ID = 0x01,
+enum amd_oem_cmds {
+  CMD_OEM_PLATFORM_ID = 0x01,
+  CMD_OEM_GET_BOOT_STRAP_ACC = 0x02,
 };
 
-enum
-{
-    AMD_IANA_ID = 0xE78,
+enum {
+  AMD_IANA_ID = 0xE78,
 };
 
-struct AMDIANAHeader
-{
-    uint8_t iana[3];
+struct AMDIANAHeader {
+  uint8_t iana[3];
 } __attribute__((packed));
 
-}
+} // namespace amd
